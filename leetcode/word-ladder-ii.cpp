c052eb4a7a1a3b005f7f681126f9a85191a7ebc3
ogail <elogeel@outlook.com>
2015-04-12 06:45:30 -0700

    Add improved solution to word-ladder-ii.cpp

--- leetcode/word-ladder-ii.cpp
+++ leetcode/word-ladder-ii.cpp
@@ -1,18 +1,18 @@
+#include <stdio.h>
 #include <unordered_set>
 #include <unordered_map>
 #include <vector>
 #include <string>
 #include <climits>
-#include <assert.h>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
 class Solution {
 public:
-    unordered_map<string, vector<string>> hash;
-
-    vector<string> getNextWords(string search, unordered_set<string> &dict) {
-        vector<string> next;
+    void getNextWords(string search, unordered_set<string> &dict, vector<string> &next) {
+        next.clear();
         for (string word : dict) {
             int diff = 0;
             int i = 0;
@@ -25,56 +25,67 @@ public:
                 next.push_back(word);
             }
         }
-
-        return next;
-    }
-
-    void findLaddersCore(
-        string current,
-        string end,
-        unordered_set<string> &dict,
-        vector<vector<string>> &found,
-        vector<string> path,
-        int &size) {
-        if (current.compare(end) == 0) {
-            if (path.size() <= size) {
-                if (path.size() < size) {
-                    found.clear();
-                    size = path.size();
-                }
-
-                found.push_back(vector<string>(path.begin(), path.end()));
-            }
-
-            return;
-        }
-
-        if (path.size() >= size) {
-            return;
-        }
-
-        vector<string> next = hash[current];
-        unordered_set<string> search(path.begin(), path.end());
-        for (string word : next) {
-            if (search.count(word) == 0) {
-                vector<string> candidate = path;
-                candidate.push_back(word);
-                findLaddersCore(word, end, dict, found, candidate, size);
-            }
-        }
     }
 
     vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
         vector<vector<string>> found;
-        int size = INT_MAX;
+        int shortest = INT_MAX;
         dict.insert(end);
+        queue<string> q;
+        vector<string> next;
+        unordered_map<string, unordered_set<string>> paths;
+        string word;
 
-        for (string word : dict) {
-            hash[word] = getNextWords(word, dict);
+        q.push(start);
+        while (!q.empty()) {
+            word = q.front();
+            q.pop();
+
+            if (word.compare(end) == 0) {
+                break;
+            }
+
+            getNextWords(word, dict, next);
+            for (string nextWord : next) {
+                paths[nextWord].insert(word);
+                q.push(nextWord);
+            }
+            dict.erase(word);
         }
-        hash[start] = getNextWords(start, dict);
 
-        findLaddersCore(start, end, dict, found, { start }, size);
+        if (paths.empty()) {
+            return found;
+        }
+
+        bool multiple;
+        do {
+            multiple = false;
+            word = end;
+            list<string> path;
+            while (word.compare(start) != 0) {
+                path.push_front(word);
+                string nextWord = *paths[word].begin();
+                if (paths[word].size() > 1) {
+                    multiple = true;
+                    paths[word].erase(nextWord);
+                }
+                word = nextWord;
+
+                if (path.size() > shortest) {
+                    break;
+                }
+            }
+            path.push_front(start);
+
+            if (path.size() <= shortest) {
+                if (path.size() < shortest) {
+                    found.clear();
+                    shortest = path.size();
+                }
+                found.push_back(vector<string>(path.begin(), path.end()));
+            }
+        } while (multiple);
+
         return found;
     }
 };
