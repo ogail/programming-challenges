f792e6f94e98905f28f06f9efd44ca7e73cc3dde
ogail <elogeel@outlook.com>
2015-04-12 14:13:53 -0700

    Optimize word-ladder-ii.cpp

--- leetcode/word-ladder-ii.cpp
+++ leetcode/word-ladder-ii.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 class Solution {
 public:
-    void getNextWords(string search, unordered_set<string> &dict, vector<string> &next) {
+    void getNextWords(string search, unordered_set<string> &dict, unordered_set<string> &next) {
         next.clear();
         
         for (int i = 0; i < search.size(); ++i) {
@@ -19,8 +19,8 @@ public:
             while (letter <= 'z') {
                 string temp = search;
                 temp[i] = letter;
-                if (dict.count(temp) != 0 && search.compare(temp) != 0) {
-                    next.push_back(temp);
+                if (dict.count(temp) != 0) {
+                    next.insert(temp);
                 }
                 letter++;
             }
@@ -29,10 +29,9 @@ public:
 
     vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
         vector<vector<string>> found;
-        int shortest = INT_MAX;
         dict.insert(end);
         queue<string> q;
-        vector<string> next;
+        unordered_set<string> next;
         unordered_map<string, unordered_set<string>> paths;
         string word;
 
@@ -45,47 +44,50 @@ public:
                 break;
             }
 
-            getNextWords(word, dict, next);
-            for (string nextWord : next) {
-                paths[nextWord].insert(word);
-                q.push(nextWord);
-            }
             dict.erase(word);
+            getNextWords(word, dict, next);
+            
+            if (next.count(end) == 0) {
+                for (string nextWord : next) {
+                    if (paths.count(nextWord) == 0) {
+                        q.push(nextWord);
+                    }
+                    paths[nextWord].insert(word);
+                }
+            }
+            else {
+                q.push(end);
+                paths[end].insert(word);
+            }
         }
 
         if (paths.empty()) {
             return found;
         }
 
-        bool multiple;
-        do {
-            multiple = false;
-            word = end;
-            list<string> path;
-            while (word.compare(start) != 0) {
-                path.push_front(word);
-                string nextWord = *paths[word].begin();
-                if (paths[word].size() > 1) {
-                    multiple = true;
-                    paths[word].erase(nextWord);
-                }
-                word = nextWord;
-
-                if (path.size() > shortest) {
-                    break;
-                }
-            }
-            path.push_front(start);
-
-            if (path.size() <= shortest) {
-                if (path.size() < shortest) {
-                    found.clear();
-                    shortest = path.size();
-                }
-                found.push_back(vector<string>(path.begin(), path.end()));
-            }
-        } while (multiple);
+        vector<string> path;
+        populatePaths(end, start, paths, path, found);
 
         return found;
+    }
+
+    void populatePaths(
+        string curr,
+        string end,
+        unordered_map<string, unordered_set<string>> &paths,
+        vector<string> &path,
+        vector<vector<string>> &found) {
+        path.push_back(curr);
+        if (curr.compare(end) == 0) {
+            found.push_back(vector<string>(path.rbegin(), path.rend()));
+            path.pop_back();
+            return;
+        }
+
+        const unordered_set<string> &neighbirs = paths[curr];
+        for (string neighbir : neighbirs) {
+            populatePaths(neighbir, end, paths, path, found);
+        }
+        path.pop_back();
     }
 };
